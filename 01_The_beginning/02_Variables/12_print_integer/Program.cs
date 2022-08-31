392188b64ba73d32b0389ae7a91219d01084048a
nikterez <108397160+nikterez@users.noreply.github.com>
2022-08-31 08:44:47 +0300

    Add files via upload

---
+++ 01_The_beginning/02_Variables/12_print_integer/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Exercise012
+{
+  public class Program
+  {
+    public static void Main(string[] args)
+    {
+      System.Console.WriteLine("Give a number: ");
+      int i = Convert.ToInt32(Console.ReadLine());
+      System.Console.WriteLine("You gave "+i);
+    }
+  }
+}
---
+++ 01_The_beginning/02_Variables/13_print_double/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Exercise013
+{
+  public class Program
+  {
+    public static void Main(string[] args)
+    {
+      System.Console.WriteLine("Give a double");
+      double d = Convert.ToDouble(Console.ReadLine());
+      System.Console.WriteLine("You gave: "+d);
+    }
+  }
+}
