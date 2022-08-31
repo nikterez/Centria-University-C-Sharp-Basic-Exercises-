fd443d18d1a8fdb9e551a5886b7a61ff0135afa3
nikterez <108397160+nikterez@users.noreply.github.com>
2022-08-31 09:08:44 +0300

    Add files via upload

---
+++ 01_The_beginning/05_Loops/40_amount_and_sum/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Exercise040
+{
+  public class Program
+  {
+    public static void Main(string[] args)
+    {
+            int sum = 0;
+            int count = 0;
+
+            while (true)
+            {
+                System.Console.WriteLine("Give number: ");
+                int input = Convert.ToInt32(Console.ReadLine());
+                if (input == 0)
+                {
+                    break;
+                }
+                sum += input;
+                count++;
+            }
+            System.Console.WriteLine("Sum: " + sum);
+            System.Console.WriteLine("Ammount of numbers: " + count);
+    }
+  }
+}
