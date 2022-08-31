ae071c01a2eff7e43b1c10b88bb39f7e101751ec
nikterez <108397160+nikterez@users.noreply.github.com>
2022-08-31 08:47:34 +0300

    Add files via upload

---
+++ 01_The_beginning/02_Variables/14_print_truth/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Exercise014
+{
+  public class Program
+  {
+    public static void Main(string[] args)
+    {
+      System.Console.WriteLine("Give me the truth");
+      bool b = Convert.ToBoolean(Console.ReadLine());
+      System.Console.WriteLine(b);
+      System.Console.WriteLine(!b);
+    }
+  }
+}
---
+++ 01_The_beginning/02_Variables/15_asking_multiple_inputs/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Exercise015
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            System.Console.Write("Give string: ");
+            string str = Console.ReadLine();
+
+            System.Console.Write("Give integer: ");
+            int i = Convert.ToInt32(Console.ReadLine());
+
+            System.Console.Write("Give double: ");
+            double d = Convert.ToDouble(Console.ReadLine());
+
+            System.Console.WriteLine("String: " + str);
+            System.Console.WriteLine("Integer: " + i);
+            System.Console.WriteLine("Doulbe: " + d);
+
+        }
+    }
+}
