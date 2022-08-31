b2fc81c22078575ce14b7c3a4005d5adcc1c2e9b
nikterez <108397160+nikterez@users.noreply.github.com>
2022-08-31 08:07:27 +0300

    Add files via upload

---
+++ 05_Loops/34_continue/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Exercise034
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            while (true)
+            {
+                System.Console.WriteLine("keep going?");
+                string inp = Console.ReadLine();
+                
+                if (inp == "no")
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
+
