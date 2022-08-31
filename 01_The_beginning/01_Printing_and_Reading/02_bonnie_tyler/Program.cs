46e69e8a0273b030d4e65e1070406f3eb7c05b7f
nikterez <108397160+nikterez@users.noreply.github.com>
2022-08-31 08:25:14 +0300

    Add files via upload

---
+++ 01_The_beginning/01_Printing_and_Reading/02_bonnie_tyler/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Exercise002
+{
+  public class Program
+  {
+    public static void Main(string[] args)
+    {
+      Console.WriteLine("Once upon a time");
+      Console.WriteLine("I was falling in love");
+      Console.WriteLine("Now I'm only falling apart");
+      //Console.Write("Once upon a time\nI was falling in love\nNow I'am only falling apart");
+    }
+  }
+}
---
+++ 01_The_beginning/01_Printing_and_Reading/03_bonnie_tyler_line_change/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Exercise003
+{
+  public class Program
+  {
+    public static void Main(string[] args)
+    {
+      Console.Write("Once upon a time\nI was falling in love\nNow I'am only falling apart");
+    }
+  }
+}
