f4ee3f45fd2f037cc5699f2ff072863835058702
nikterez <108397160+nikterez@users.noreply.github.com>
2022-08-31 09:04:24 +0300

    Add files via upload

---
+++ 01_The_beginning/05_Loops/36_power_of_positivity/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Exercise036
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            while (true)
+            {
+                System.Console.WriteLine("Give a number: ");
+                int inp = Convert.ToInt16(Console.ReadLine());
+               
+                if (inp == 0)
+                {
+                    break;
+                }
+                if (inp < 0)
+                {
+                    System.Console.WriteLine("This is negative");
+                    continue;
+                }
+                System.Console.WriteLine(inp*inp);
+                //System.Console.WriteLine(Math.Pow(inp, 2));
+            }
+        }
+    }
+}
